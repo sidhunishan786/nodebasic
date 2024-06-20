324d6da79f0ea9734d75a420d0e63f3a1b1c7827
nishan singh <sidhunishan786@gmail.com>
2024-06-20 08:12:13 +0530

    day2

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-const math = require('fs');
-console.log(math);
+const fs = require('fs');
+const http = require('http');
+
+const server=http.createServer((req,res)=>{
+    var data=Date.now().toString()+"\n";
+    fs.appendFile('./temp.txt',data,()=>{
+
+    });
+    res.end("appended file");
+    console.log(req);
+})
+
+server.listen(8000,()=>{
+    
+    console.log("running..............");
+
+})
