8efe08d0b0a36600e6355d7521a4a9e84597893c
nishan singh <sidhunishan786@gmail.com>
2024-06-27 23:02:24 +0530

    27thJune2024

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,39 @@
 const express = require('express');
-
+const data = require('./MOCK_DATA.json');
 const app = express();
 
-app.get('/home',(req,res)=>{
+app.get('/api/users',(req,res)=>{
     // console.log(req.path);
-    res.json("you are at "+ req.path);
+    // const html = `${data.map((curr)=> "<h1> first Name - "+curr.first_name+"</h1>").join("")}`;
+    res.send(data);
 
+});
+
+let arr=[]
+
+app.route('/users').get((req,res)=>{
+    // console.log(req.path);
+    const html = `${data.map((curr)=> "<h1> first Name - "+curr.first_name+"</h1>").join("")}`;
+    res.send(html);
+
+})
+.post((req,res)=>{
+    arr.push(req.body);
+    console.log(arr);
+    res.json(arr.length);
 });
 
 app.get('',(req,res)=>{
     // console.log(req.path);
-    res.end("you are at "+ req.path);
+    res.end("you are at HOME");
     
 });
+
+app.route('/user/:id').get((req,res)=>{
+    return res.json(data.filter((curr)=> curr.id===Number(req.params.id)));
+}).patch((req,res)=>{
+    return res.send("done");
+})
 
 
 app.listen(8000,()=>{
