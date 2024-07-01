772f4406316ca9c0cc18a976ca1391fd65b1a994
nishan singh <sidhunishan786@gmail.com>
2024-07-01 21:44:16 +0530

    1JULY2024

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,39 @@ let data = require('./MOCK_DATA.json');
 const app = express();
 const fs = require('fs');
 const { stringify } = require('querystring');
+const mongoose = require('mongoose');
+const { log, timeStamp } = require('console');
+
+mongoose.connect('mongodb://127.0.0.1:27017/firstapp')
+.then(()=> console.log("connected"))
+.catch((err)=> console.log("error while connecting to DB ERR- ",err));
+
+
+const userSchema = mongoose.Schema({
+    firstName :{
+        type: String,
+        required : true,
+    },
+    lastName : {
+        type: String,
+        required : false
+    },
+    gender :{
+        type: String
+    },
+    profession:{
+        type: String
+    },
+    email :{
+        required : true,
+        unique : true,
+        type: String
+    }
+}
+,{timestamps : true});
+
+const User = mongoose.model("user",userSchema);
+
 
 app.use(express.json());
 app.use(express.json({ extended: true }));
@@ -16,22 +49,45 @@ app.get('/api/users',(req,res)=>{
 
 
 app.route('/users')
-.get((req,res)=>{
+.get(async (req,res)=>{
     // console.log(req.path);
-    const html = `${data.map((curr)=> "<h1> first Name - "+curr.first_name+"</h1>").join("")}`;
-    res.send(html);
+    const users = await User.find({});
+    
+   
+
+    console.log(users);
+    const html = `${users.map((curr)=> "<h1> first Name - "+curr.firstName+"</h1>  <h2> email - " + curr.email + "</h2>").join("")}`;
+    return res.send(html);
+
+
+    
 
 })
-.post((req,res)=>{
-    let y=data[data.length-1].id;
-   
-    // console.log(req.body);
-    data.push({id: y+1 , ...req.body});
-    fs.writeFile('./MOCK_DATA.json',JSON.stringify(data),(err)=>{
-        console.log('error occurred!!');
-    })
+.post(async (req,res)=>{
+    
+    const result = await User.create({
+        firstName : req.body.first_name,
+        lastName : req.body.last_name,
+        gender : req.body.gender,
+        email : req.body.email
+    });
 
-    res.status(202).json(y+1);
+    console.log(result);
+
+    return res.status(201).json({status: "success"});
+
+    
+    // let y=data[data.length-1].id;
+
+
+   
+    // // console.log(req.body);
+    // data.push({id: y+1 , ...req.body});
+    // fs.writeFile('./MOCK_DATA.json',JSON.stringify(data),(err)=>{
+    //     console.log('error occurred!!');
+    // })
+
+    // res.status(202).json(y+1);
 });
 
 app.get('',(req,res)=>{
@@ -40,9 +96,19 @@ app.get('',(req,res)=>{
     
 });
 
-app.route('/user/:id').get((req,res)=>{
-    return res.json(data.filter((curr)=> curr.id===Number(req.params.id)));
-}).patch((req,res)=>{
+app.route('/user/:id').get(async (req,res)=>{
+
+    const puser = await User.findById(req.params.id);
+
+
+    return res.json(puser);
+
+
+}).patch(async (req,res)=>{
+
+   
+
+    
     for(let x=0;x<data.length;x++){
         if(data[x].id==Number(req.params.id)){
             data[x].first_name = req.body.first_name;
@@ -75,14 +141,20 @@ app.route('/user/:id').get((req,res)=>{
    return res.status(203).json("done");
 
 })
-.delete((req,res)=>{
-    data=data.filter((curr)=> curr.id!=Number(req.params.id));
-    fs.writeFile('./MOCK_DATA.json',JSON.stringify(data),(err)=>{
-        console.log('error occurred!!');
-    });
-    res.setHeader('x-name','Nishan');
+.delete(async (req,res)=>{
 
-    return res.status(203).json("done");
+    await User.findByIdAndDelete(req.params.id);
+    
+    return res.json("success");
+
+
+    // data=data.filter((curr)=> curr.id!=Number(req.params.id));
+    // fs.writeFile('./MOCK_DATA.json',JSON.stringify(data),(err)=>{
+    //     console.log('error occurred!!');
+    // });
+    // res.setHeader('x-name','Nishan');
+
+    // return res.status(203).json("done");
 })
 
 
