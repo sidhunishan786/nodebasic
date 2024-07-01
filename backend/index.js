95860d50f280e0bad81b78af2ee54a93bb2138c2
nishan singh <sidhunishan786@gmail.com>
2024-07-01 20:48:00 +0530

    1JULY2024

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const data = require('./MOCK_DATA.json');
+let data = require('./MOCK_DATA.json');
 const app = express();
+const fs = require('fs');
+const { stringify } = require('querystring');
 
-
+app.use(express.json());
+app.use(express.json({ extended: true }));
 app.get('/api/users',(req,res)=>{
     // console.log(req.path);
     // const html = `${data.map((curr)=> "<h1> first Name - "+curr.first_name+"</h1>").join("")}`;
@@ -10,18 +13,25 @@ app.get('/api/users',(req,res)=>{
 
 });
 
-let arr=[]
 
-app.route('/users').get((req,res)=>{
+
+app.route('/users')
+.get((req,res)=>{
     // console.log(req.path);
     const html = `${data.map((curr)=> "<h1> first Name - "+curr.first_name+"</h1>").join("")}`;
     res.send(html);
 
 })
 .post((req,res)=>{
-    arr.push(req.body);
-    console.log(arr);
-    res.json(arr.length);
+    let y=data[data.length-1].id;
+   
+    // console.log(req.body);
+    data.push({id: y+1 , ...req.body});
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(data),(err)=>{
+        console.log('error occurred!!');
+    })
+
+    res.status(202).json(y+1);
 });
 
 app.get('',(req,res)=>{
@@ -33,7 +43,46 @@ app.get('',(req,res)=>{
 app.route('/user/:id').get((req,res)=>{
     return res.json(data.filter((curr)=> curr.id===Number(req.params.id)));
 }).patch((req,res)=>{
-    return res.send("done");
+    for(let x=0;x<data.length;x++){
+        if(data[x].id==Number(req.params.id)){
+            data[x].first_name = req.body.first_name;
+
+        }
+    }
+
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(data),(err)=>{
+        console.log('error occurred!!');
+    })
+
+   return res.status(203).json("done");
+
+
+})
+.put((req,res)=>{
+    let y=Number(req.params.id);
+    for (let index = 0; index < data.length; index++) {
+        if(index==y-1){
+            data[y-1]={id: data[y-1].id, ...req.body};
+        }
+
+        
+    }
+
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(data),(err)=>{
+        console.log('error occurred!!');
+    })
+
+   return res.status(203).json("done");
+
+})
+.delete((req,res)=>{
+    data=data.filter((curr)=> curr.id!=Number(req.params.id));
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(data),(err)=>{
+        console.log('error occurred!!');
+    });
+    res.setHeader('x-name','Nishan');
+
+    return res.status(203).json("done");
 })
 
 
